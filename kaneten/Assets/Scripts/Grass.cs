45a5a11991c98914f2f3ecf25dd39aa18ace85c2
h_miyamoto <h_miyamoto@r-jc.jp>
2019-09-10 18:49:05 +0900

    画面スクロール(背景)作成

---
+++ kaneten/Assets/Scripts/Grass.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Grass : MonoBehaviour
+{
+    const float scale = 24.0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // 左方向へ0.1ずつ移動させる
+        transform.Translate(-0.1f, 0, 0);
+        // 画像サイズ分左へ行った場合
+        if (transform.position.x < -scale)
+        {
+            // 1画面分右に移動する
+            transform.position = new Vector3(scale, transform.position.y, 0);
+            
+        }
+    }
+}
