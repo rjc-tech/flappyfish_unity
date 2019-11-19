3015e0649c6efe66be3e92215a4a5732e955bcf0
h_miyamoto <h_miyamoto@r-jc.jp>
2019-11-19 13:02:13 +0900

    背景、障害物スクロール追加

--- kaneten/Assets/Scripts/ScrollObject.cs
+++ kaneten/Assets/Scripts/ScrollObject.cs
@@ -2,27 +2,41 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Grass : MonoBehaviour
+public class ScrollObject : MonoBehaviour
 {
-    const float scale = 24.0f;
+
+
+	const float scale = 25.0f;	
+	const float height = 8.0f;
+	
+	
+	public GameObject originObject;
+	
+	float y = 0.05f;
+	
 
     // Start is called before the first frame update
     void Start()
     {
-        
+ 
     }
 
     // Update is called once per frame
     void Update()
     {
-        // 左方向へ0.1ずつ移動させる
+         // 左方向へ0.1ずつ移動させる
         transform.Translate(-0.1f, 0, 0);
+        
         // 画像サイズ分左へ行った場合
         if (transform.position.x < -scale)
         {
             // 1画面分右に移動する
-            transform.position = new Vector3(scale, transform.position.y, 0);
+            //transform.position = new Vector3(scale, transform.position.y, 0);
             
+             //GameObjectを削除
+             Object.Destroy(gameObject, 0f);
+          
         }
+        
     }
 }
